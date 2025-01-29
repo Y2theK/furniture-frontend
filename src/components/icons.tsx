86ff7cd69bd228d698402ff6e4505af96c0938d8
Y2theK <yeyintkyaw.dev@gmail.com>
2025-01-29 22:53:54 +0630

    feat: carts

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,7 +7,7 @@ import {
   GearIcon,
   ExitIcon,
 } from "@radix-ui/react-icons";
-import { ArrowLeft, HeartIcon, Layers, MinusIcon, PlusIcon, StarIcon } from "lucide-react";
+import { ArrowLeft, HeartIcon, Layers, MinusIcon, PlusIcon, StarIcon, TrashIcon } from "lucide-react";
 
 export type IconProps = React.HTMLAttributes<SVGElement>;
 export const Icons = {
@@ -40,4 +40,13 @@ export const Icons = {
   dashboard: DashboardIcon,
   gear: GearIcon,
   exit: ExitIcon,
+  trash: TrashIcon,
+  cart: (props: IconProps) => {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" {...props} >
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+      </svg>
+
+    );
+  }
 };
