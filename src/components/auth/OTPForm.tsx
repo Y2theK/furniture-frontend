65a0d1b4f964fb5ab3df184374c2ddd54ba0a4ae
Y2theK <yeyintkyaw.dev@gmail.com>
2025-04-19 23:23:52 +0630

    feat: react hook from in otp and confirm password

--- src/components/auth/OTPForm.tsx
+++ src/components/auth/OTPForm.tsx
@@ -15,56 +15,116 @@ import {
 import {
   InputOTP,
   InputOTPGroup,
+  InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import {
+  Link,
+  useActionData,
+  useNavigation,
+  useSubmit,
+} from "react-router-dom";
+import { REGEXP_ONLY_DIGITS } from "input-otp";
+import { Icons } from "../icons";
+import { cn } from "@/lib/utils";
 
-const FormSchema = z.object({
-  pin: z.string().min(6, {
+const formSchema = z.object({
+  otp: z.string().min(6, {
     message: "Your one-time password must be 6 characters.",
   }),
 });
 
-export function InputOTPForm() {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
+export function InputOTPForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
-      pin: "",
+      otp: "",
     },
   });
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {}
+  const submit = useSubmit();
+  const actionData = useActionData() as {
+    error?: string;
+    message?: string;
+  };
+  const errorMessage = actionData?.error || null;
+  const navigation = useNavigation();
 
+  const isSubmitting = navigation.state === "submitting";
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // pass the form data to the action function
+    submit(values, {
+      method: "POST",
+      action: "register/otp",
+    });
+  }
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
-          control={form.control}
-          name="pin"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>One-Time Password</FormLabel>
-              <FormControl>
-                <InputOTP maxLength={6} {...field}>
-                  <InputOTPGroup>
-                    <InputOTPSlot index={0} />
-                    <InputOTPSlot index={1} />
-                    <InputOTPSlot index={2} />
-                    <InputOTPSlot index={3} />
-                    <InputOTPSlot index={4} />
-                    <InputOTPSlot index={5} />
-                  </InputOTPGroup>
-                </InputOTP>
-              </FormControl>
-              <FormDescription>
-                Please enter the one-time password sent to your phone.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col items-center gap-2">
+          <Link to="#" className="flex flex-col items-center gap-2 font-medium">
+            <div className="flex h-8 w-8 items-center justify-center rounded-md">
+              <Icons.logo className="size-6" />
+            </div>
+            <span className="sr-only">Furniture Shop.</span>
+          </Link>
+          <h1 className="mb-8 text-xl font-bold">
+            We have sent OTP to your phone
+          </h1>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-2/3 space-y-6"
+            >
+              <FormField
+                control={form.control}
+                name="otp"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <InputOTP
+                        maxLength={6}
+                        {...field}
+                        pattern={REGEXP_ONLY_DIGITS}
+                      >
+                        <InputOTPGroup>
+                          <InputOTPSlot index={0} />
+                          <InputOTPSlot index={1} />
+                        </InputOTPGroup>
+                        <InputOTPSeparator />
+                        <InputOTPGroup>
+                          <InputOTPSlot index={2} />
+                          <InputOTPSlot index={3} />
+                        </InputOTPGroup>
+                        <InputOTPSeparator />
+                        <InputOTPGroup>
+                          <InputOTPSlot index={4} />
+                          <InputOTPSlot index={5} />
+                        </InputOTPGroup>
+                      </InputOTP>
+                    </FormControl>
+                    <FormDescription>
+                      Please enter the one-time password sent to your phone.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+              {actionData && (
+                <p className="text-red-400">{actionData?.message}</p>
+              )}
+              <Button type="submit" className="w-full">
+                {isSubmitting ? "Submitting..." : "Verify OTP"}
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,7 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Icons } from "../icons";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
